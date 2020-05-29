e81717b58b03d51a73abca40bbd7802526baa0dd
ErikaAlbq <erica.albuquerque@fatec.sp.gov.br>
2020-05-29 16:00:05 -0300

    Atividade2-ex-1

--- componentes/estilos.js
+++ componentes/estilos.js
@@ -1,43 +1,46 @@
 const estilos = {
   container: {
     display: 'block',
-  },
-  titulo: {
-    textAlign: 'center',
-    marginBottom: 16,
     fontFamily: 'Comic Sans MS',
+  },
+  rotulo:{
+    marginBottom: 5,
+    fontSize: 12,
+
   },
   linha: {
     display: 'flex',
     alignItems: 'center',
     flexDirection: 'row',
-    justifyContent: 'center',
-  },
-  botao_pessoas: {
-    backgroundColor: "#303BD3",
-    padding: 20,
-    marginRight: 25,
-    marginLeft: 10,
+    justifyContent: "center",
+    marginTop: 10
+    },
+
+  botao_tecla: {
+    backgroundColor: "#0A0A2A",
+    padding: 10,
+    marginHorizontal: 5,
+    marginLeft: 5,
     borderWidth: 1,
     borderRadius: 5,
-    color: "#000",
+    color: "#FFFFFF",
     fontWeigth: 'bold',
+     minWidth: 35,
     fontFamily: 'Comic Sans MS',
     fontSize: 20
-  }
+  },
+
+  display: {
+    color: '#0B0B61',
+    fontWeight: 'bold',
+    backgroundColor: '#FFFF00',
+    padding: 10,
+    borderRadius: 8,
+    textAlign: 'right',
+    height: 20
+  },
  
-
-
-
-
-
-
-
+  
 };
-
-
-
-
-
 
 export default estilos; 
--- index.js
+++ index.js
@@ -1,40 +1,65 @@
-import React, { Component } from 'react';
+import React, { Component,props } from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
 import estilos from "./componentes/estilos";
-import Pessoa from "./componentes/Pessoa";
+import Teclas from "./componentes/Teclas";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      q_clicou: 'Ninguem',
+      display : '',
       
-    };
-  }
+    }
 
- alterar = (origem)=>{
-   this.setState({
-     q_clicou: origem
-   });
- 
- }
-
- 
-  render() {
+this.linha1 = [0,1,2,3,4,5,6,7].map(item =>
+<div>
+   <Teclas t={item} setar ={this.concatenar} digito={item}/>
     
+   </div>
+);
+
+ this.linha2 = [8,9,'A','B','C','D','E','F'].map(item =>
+<div>
+   <Teclas t={item} setar ={this.concatenar} digito={item}/>
+  
+   </div>
+ );
+
+
+
+
+ 
+
+}
+
+concatenar =(digito)=> {
+    const aux  = this.state.display;
+    aux += digito;
+    this.setState({
+      display: aux
+    });
+  }
+ 
+
+  render() {
     return (
-   <div style={estilos.container}>
-         <p  style={estilos.titulo}>Quem clicou? {this.state.q_clicou}</p>
-         
-         <div style={estilos.linha }>
-         
-            <Pessoa pessoa= "ELA" alterar={this.alterar}  />
-            <Pessoa pessoa= "ELE" alterar={this.alterar}  />
+      <div style={estilos.container}>
+        <div style={estilos.rotulo}>Display</div>
         
-          </div>
+        <div style={estilos.display}>
+          {this.state.display}
         </div>
-      
+
+        <div style={estilos.linha}>
+          {this.linha1}
+        </div>
+
+        <div style={estilos.linha}>
+          {this.linha2}
+        </div>
+      </div>
+
     );
   }
 }
